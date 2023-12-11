212a32ee551a88fd6356558b26b07129aad9f26b
WarlockHolmez <warlockholmez@gmail.com>
2023-12-11 12:19:44 -0500

    Update DamageEvent.cs

--- Source/ACE.Server/Entity/DamageEvent.cs
+++ Source/ACE.Server/Entity/DamageEvent.cs
@@ -389,7 +389,7 @@ namespace ACE.Server.Entity
             // ---- ARMOR ----
             var armorRendingMod = 1.0f;
             if (Weapon != null && Weapon.HasImbuedEffect(ImbuedEffectType.ArmorRending))
-                armorRendingMod = WorldObject.GetArmorRendingMod(attackSkill);
+                armorRendingMod = armorRendingMod - WorldObject.GetArmorRendingMod(attackSkill);
 
             var armorCleavingMod = attacker.GetArmorCleavingMod(Weapon);
 
