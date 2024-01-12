360f79105df241e890b6f199892b04ad9c952a09
WarlockHolmez <warlockholmez@gmail.com>
2024-01-12 14:05:10 -0500

    fix: additional fixes

--- Source/ACE.Server/Factories/LootGenerationFactory_Spells.cs
+++ Source/ACE.Server/Factories/LootGenerationFactory_Spells.cs
@@ -53,7 +53,8 @@ namespace ACE.Server.Factories
                         }
                         if (wo.W_WeaponType == WeaponType.Bow || wo.W_WeaponType == WeaponType.Crossbow || wo.W_WeaponType == WeaponType.Thrown)
                         {
-                            wo.DamageMod = (int)(wo.DamageMod.Value * 0.75f);
+                            Console.WriteLine($"{wo.Name} {wo.DamageMod.Value}");
+                            wo.DamageMod = (wo.DamageMod.Value * 0.75f);
                         }
 
                         numSpells++;
