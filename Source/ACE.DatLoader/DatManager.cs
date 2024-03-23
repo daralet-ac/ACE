badd2bc58b3b9244c9e41453b358afdff8fe6157
WarlockHolmez <warlockholmez@gmail.com>
2024-03-22 22:09:25 -0400

    feat: change spell name (new portal.dat)
    * Change "Augmented Understanding" spell names to:
       * Rested
       * Well Rested
       * Vert Well Rested
    * Requires portal.dat (30016)

--- Source/ACE.DatLoader/DatManager.cs
+++ Source/ACE.DatLoader/DatManager.cs
@@ -12,7 +12,7 @@ namespace ACE.DatLoader
         private static int count;
 
         private static int ITERATION_CELL = 30003;
-        private static int ITERATION_PORTAL = 30015;
+        private static int ITERATION_PORTAL = 30016;
         private static int ITERATION_HIRES = 497;
         private static int ITERATION_LANGUAGE = 30005;
         public static CellDatDatabase CellDat { get; private set; }
