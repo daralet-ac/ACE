8c0e6e6804fbe785ef4ec2ac1d961c5c19c0ff6e
Your Name <roosterjcogburn@gmail.com>
2024-04-01 21:30:11 -0400

    update: death penalty and corpse logs
    - removes equipped items from player death items
    - increases decay time on player corpses to 30 mins per level
    - adds player corpselog and command "corpses" which displays a list of current corpses.
    - on use of any of the @corpses commands, corpses that no longer exist in the world are removed from the log before the list is sent to players
    - players may also manually remove corpses from their log with @corpses remove [number].
    - additionally players may query which items they dropped on a particular corpse with @corpses [number]
    -
    - makes capstone dungeons no drop (would be too fiddly to manage corpse recovery + permissions)
    -

--- Source/ACE.Server/Command/Handlers/PlayerCommands.cs
+++ Source/ACE.Server/Command/Handlers/PlayerCommands.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using ACE.Common;
 using ACE.Database;
 using ACE.Entity;
@@ -11,7 +13,9 @@ using ACE.Server.Network;
 using ACE.Server.Network.GameEvent.Events;
 using ACE.Server.Network.GameMessages.Messages;
 using ACE.Server.WorldObjects;
+using MySqlX.XDevAPI.Common;
 using Serilog;
+
 
 namespace ACE.Server.Command.Handlers
 {
@@ -519,6 +523,133 @@ namespace ACE.Server.Command.Handlers
             msg += "\n\n\n\n";
 
             session.Network.EnqueueSend(new GameMessageSystemChat(msg, ChatMessageType.AdminTell));
+        }
+
+        [CommandHandler("corpses", AccessLevel.Player, CommandHandlerFlag.RequiresWorld, "Shows location of recent player corpses on the landscape")]
+        public static void CorpseList(Session session, params string[] parameters)
+        {
+            if (session.Player.CorpseLog == null || session.Player.CorpseLog.Length == 0)
+            {
+                session.Network.EnqueueSend(new GameMessageSystemChat("You have no recent corpses.", ChatMessageType.Broadcast));
+                return;
+            }
+
+            var corpses = session.Player.CorpseLog.Split(';');
+
+            if (corpses != null)
+            {
+                var rewrittenCorpseLog = "";
+
+                // MAINTENANCE STEP: First we query the DB to see if any of the corpses no longer exist, and remove them from the list if so
+                foreach (var corpselog in corpses)
+                {
+                    var corpse = corpselog.Split('|');
+
+                    if (corpse.Length > 0)
+                    {
+                        // parse corpse guid, query DB to see if it still exists, if it doesn't do not add to the rewritten string
+                        if (uint.TryParse(corpse[0], out var corpseGuid))
+                        {
+                            var exists = DatabaseManager.Shard.BaseDatabase.GetBiota(corpseGuid);
+                            if (exists != null && exists.WeenieType == (int)WeenieType.Corpse)
+                                rewrittenCorpseLog += string.Join("|", corpse) + ";";
+                        }
+                    }
+                }
+
+                session.Player.CorpseLog = rewrittenCorpseLog;
+
+                // check again to see if, after removal, we have any corpses in the log
+                if (session.Player.CorpseLog == null || session.Player.CorpseLog.Length == 0)
+                {
+                    session.Network.EnqueueSend(new GameMessageSystemChat("You have no recent corpses.", ChatMessageType.Broadcast));
+                    return;
+                }
+
+                if (parameters.Length > 0)
+                {
+                    // QUERY DROPPED LOOT: @corpses [number]
+                    if (parameters.Length == 1)
+                    {
+                        if (int.TryParse(parameters[0], out var result))
+                        {
+                            var corpseLogs = session.Player.CorpseLog.Split(';');
+                            if (corpseLogs.Length >= result)
+                            {
+                                var log = corpseLogs[result - 1];
+                                var split = log.Split('|');
+                                if (split.Length == 6)
+                                {
+                                    session.Network.EnqueueSend(new GameMessageSystemChat($"Corpse {result}: {split[5]}", ChatMessageType.Broadcast));
+                                    return;
+                                }
+                            }
+                            else
+                            {
+                                session.Network.EnqueueSend(new GameMessageSystemChat($"Invalid corpse number.", ChatMessageType.Broadcast));
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            session.Network.EnqueueSend(new GameMessageSystemChat("That is not a valid command.", ChatMessageType.Broadcast));
+                            return;
+                        }
+
+                    }
+
+                    // REMOVE CORPSE FROM LIST: @corpse remove [number]
+                    if (parameters.Length == 2)
+                    {
+                        if (parameters[0].ToLower() == "remove")
+                        {
+                            if (int.TryParse(parameters[1], out var result))
+                            {
+                                var corpseLogs = session.Player.CorpseLog.Split(';');
+                                if (corpseLogs.Length >= result)
+                                {
+                                    string newCorpseLog = string.Join(";", corpseLogs.Where(p => p != corpseLogs[result - 1]));
+                                    session.Player.CorpseLog = newCorpseLog;
+                                    session.Network.EnqueueSend(new GameMessageSystemChat($"Corpse {result} has been removed, however the corpse still exists and may be retrieved.", ChatMessageType.Broadcast));
+                                    return;
+                                }
+                                else
+                                {
+                                    session.Network.EnqueueSend(new GameMessageSystemChat($"Invalid corpse number.", ChatMessageType.Broadcast));
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // LIST OF CURRENT CORPSES: @corpses
+                else if (parameters.Length == 0)
+                {
+                    var count = 1;
+
+                    session.Network.EnqueueSend(new GameMessageSystemChat($"---------------------------Current Corpses---------------------------", ChatMessageType.Broadcast));
+
+                    var revisedCorpses = session.Player.CorpseLog.Split(";");
+
+                    foreach (var corpse in revisedCorpses)
+                    {
+                        if (corpse.Length == 0) continue;
+
+                        var split = corpse.Split("|");
+
+                        if (split == null || split.Length < 5) continue;
+
+                        // Corpse GUID, Killer Name, DateTime of Death, Location, DateTime Decay, Items
+                        session.Network.EnqueueSend(new GameMessageSystemChat($"{count}:  Killed By - {split[1]}  |  Time of Death - {split[2]}  |  Location - {split[3]}  |   Est. Decay - {split[4]}", ChatMessageType.Broadcast));
+
+                        count++;
+
+                    }
+
+                    session.Network.EnqueueSend(new GameMessageSystemChat($"Corpses will be removed from this list once opened, or once the decay time expires, though depending on landblock activity they may still exist in the world.", ChatMessageType.Broadcast));
+                }
+            }
         }
     }
 }
