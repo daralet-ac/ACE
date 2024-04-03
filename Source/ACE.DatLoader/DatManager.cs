29800d5737078645a2a485ab0a816d5c662194ad
WarlockHolmez <105719338+WarlockHolmez@users.noreply.github.com>
2024-04-03 00:05:03 -0400

    feat: portal.dat (30018) (#182)
    * new icons for combat abilities

--- Source/ACE.DatLoader/DatManager.cs
+++ Source/ACE.DatLoader/DatManager.cs
@@ -12,7 +12,7 @@ namespace ACE.DatLoader
         private static int count;
 
         private static int ITERATION_CELL = 30004;
-        private static int ITERATION_PORTAL = 30017;
+        private static int ITERATION_PORTAL = 30018;
         private static int ITERATION_HIRES = 497;
         private static int ITERATION_LANGUAGE = 30005;
         public static CellDatDatabase CellDat { get; private set; }
