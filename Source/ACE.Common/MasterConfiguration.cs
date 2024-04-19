efa6205f85efb87f6663e97b0bdd19b2ae147a88
WarlockHolmez <105719338+WarlockHolmez@users.noreply.github.com>
2024-04-19 01:24:07 -0400

    feat: move dat file iterations to config file (#198)

--- Source/ACE.Common/MasterConfiguration.cs
+++ Source/ACE.Common/MasterConfiguration.cs
@@ -9,5 +9,7 @@ namespace ACE.Common
         public OfflineConfiguration Offline { get; set; } = new OfflineConfiguration();
 
         public DDDConfiguration DDD { get; set; } = new DDDConfiguration();
+
+        public DATConfiguration DAT { get; set; } = new DATConfiguration();
     }
 }
