51e5d18864a50d954c87f5139c58de5d0d3c7bfb
Stephen Cavaliere <stephencavaliere@gmail.com>
2024-04-20 11:26:46 -0400

    feat: add gamecast to discord bot

---
+++ Source/ACE.Server/Discord/Modules/ChatModule.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using ACE.Entity.Enum;
+using ACE.Server.Managers;
+using ACE.Server.Network.GameMessages.Messages;
+using Discord.Interactions;
+
+namespace ACE.Server.Discord.Modules;
+
+public class ChatModule : InteractionModuleBase<SocketInteractionContext>
+{
+    [RequireRole("Admin")]
+    [SlashCommand("gamecast", "Broadcast messages to the world on behave of SYSTEM")]
+    public async Task Gamecast(string message)
+    {
+        var msg = $"<Broadcast from System> {message}";
+        var sysMessage = new GameMessageSystemChat(msg, ChatMessageType.WorldBroadcast);
+        PlayerManager.BroadcastToAll(sysMessage);
+        PlayerManager.LogBroadcastChat(Channel.AllBroadcast, null, msg);
+
+        await RespondAsync("Message successfully broadcast to server.", ephemeral: true);
+    }
+}
