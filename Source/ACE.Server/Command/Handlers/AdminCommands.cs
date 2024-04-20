34f6725732a6627e7225cec2b5e64c4f33d5c8ca
Stephen Cavaliere <stephencavaliere@gmail.com>
2024-04-19 21:38:53 -0400

    feat: add new allow_ip command to temporarily add ips to allow list

--- Source/ACE.Server/Command/Handlers/AdminCommands.cs
+++ Source/ACE.Server/Command/Handlers/AdminCommands.cs
@@ -6,6 +6,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading;
 using ACE.Common;
 using ACE.Database;
@@ -39,6 +40,39 @@ namespace ACE.Server.Command.Handlers
         // {
         //     //TODO: output
         // }
+
+        // allow_ip {ipv4}
+        [CommandHandler("allow_ip", AccessLevel.Admin, CommandHandlerFlag.None, 1, "Temporarily add an IP to the allow list", "< ipv4 address >")]
+        public static void HandleAllowIp(Session session, params string[] parameters)
+        {
+            var ip = parameters[0];
+
+            if (string.IsNullOrWhiteSpace(ip))
+            {
+                ChatPacket.SendServerMessage(session, $"Cannot add IP: `{ip}`. Please enter a valid IPv4 address.", ChatMessageType.Broadcast);
+                return;
+            }
+
+            var regex = new Regex(@"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$");
+            if (!regex.IsMatch(ip))
+            {
+                ChatPacket.SendServerMessage(session, $"Cannot add IP: `{ip}`. Please enter a valid IPv4 address.", ChatMessageType.Broadcast);
+                return;
+            }
+
+            var allowList = ConfigManager.Config.Server.Network.AllowUnlimitedSessionsFromIPAddresses;
+            if (allowList.Contains(ip))
+            {
+                ChatPacket.SendServerMessage(session, $"IP `{ip}` already added to allow list.", ChatMessageType.Broadcast);
+                return;
+            }
+
+            Array.Resize(ref allowList, allowList.Length + 1);
+            allowList[^1] = ip;
+            ConfigManager.Config.Server.Network.AllowUnlimitedSessionsFromIPAddresses = allowList;
+
+            ChatPacket.SendServerMessage(session, $"Successfully added `{ip}` to the server allow list. **Reminder:** This is temporary and should be added to the config", ChatMessageType.Broadcast);
+        }
 
         // adminvision { on | off | toggle | check}
         [CommandHandler("adminvision", AccessLevel.Sentinel, CommandHandlerFlag.RequiresWorld, 1,
