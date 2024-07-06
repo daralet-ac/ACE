096ec5bebf8adc3863b5029a360a3224ad69b147
Stephen Cavaliere <stephencavaliere@gmail.com>
2024-07-05 21:40:54 -0400

    refactor(database-tests): apply csharpier and dotnet format suggestions

--- libs/database-tests/WeenieSearchTests.cs
+++ libs/database-tests/WeenieSearchTests.cs
@@ -1,52 +1,57 @@
 using System;
 using System.IO;
 using System.Linq;
-
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 using ACE.Common;
 using ACE.Database.Models.World;
 using ACE.Entity.Enum.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace ACE.Database.Tests
+namespace ACE.Database.Tests;
+
+[TestClass]
+public class WeenieSearchTests
 {
-    [TestClass]
-    public class WeenieSearchTests
+    private static WorldDatabase worldDb;
+
+    [ClassInitialize]
+    public static void TestSetup(TestContext context)
     {
-        private static WorldDatabase worldDb;
+        // copy config.js
+        File.Copy(
+            Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\..\\..\\ACE.Server\\Config.js"),
+            ".\\Config.js",
+            true
+        );
 
-        [ClassInitialize]
-        public static void TestSetup(TestContext context)
-        {
-            // copy config.js
-            File.Copy(Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\..\\..\\ACE.Server\\Config.js"), ".\\Config.js", true);
+        ConfigManager.Initialize();
+        worldDb = new WorldDatabase();
+    }
 
-            ConfigManager.Initialize();
-            worldDb = new WorldDatabase();
-        }
+    [TestMethod]
+    public void GetWeenie_Pyreal_ById_ReturnsObject()
+    {
+        var result = worldDb.GetWeenie(273);
 
-        [TestMethod]
-        public void GetWeenie_Pyreal_ById_ReturnsObject()
-        {
-            var result = worldDb.GetWeenie(273);
+        var stringName = result
+            .WeeniePropertiesString.FirstOrDefault(x => x.Type == (ushort)PropertyString.Name)
+            ?.Value;
 
-            var stringName = result.WeeniePropertiesString.FirstOrDefault(x => x.Type == (ushort)PropertyString.Name)?.Value;
+        Assert.IsNotNull(result);
+        Assert.IsTrue(stringName == "Pyreal");
+        Assert.IsTrue(result.GetProperty(PropertyString.Name) == "Pyreal");
+    }
 
-            Assert.IsNotNull(result);
-            Assert.IsTrue(stringName == "Pyreal");
-            Assert.IsTrue(result.GetProperty(PropertyString.Name) == "Pyreal");
-        }
+    [TestMethod]
+    public void GetWeenie_Pyreal_ByName_ReturnsObject()
+    {
+        var result = worldDb.GetWeenie("coinstack");
 
-        [TestMethod]
-        public void GetWeenie_Pyreal_ByName_ReturnsObject()
-        {
-            var result = worldDb.GetWeenie("coinstack");
+        var stringName = result
+            .WeeniePropertiesString.FirstOrDefault(x => x.Type == (ushort)PropertyString.Name)
+            ?.Value;
 
-            var stringName = result.WeeniePropertiesString.FirstOrDefault(x => x.Type == (ushort)PropertyString.Name)?.Value;
-
-            Assert.IsNotNull(result);
-            Assert.IsTrue(stringName == "Pyreal");
-            Assert.IsTrue(result.GetProperty(PropertyString.Name) == "Pyreal");
-        }
+        Assert.IsNotNull(result);
+        Assert.IsTrue(stringName == "Pyreal");
+        Assert.IsTrue(result.GetProperty(PropertyString.Name) == "Pyreal");
     }
 }
