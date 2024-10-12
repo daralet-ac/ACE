889f6b871664cc65ec6a794864b37ac45fadd587
WarlockHolmez <105719338+WarlockHolmez@users.noreply.github.com>
2024-10-12 14:02:14 -0400

    feat: add server property to test different mana costs with ease (#438)

--- apps/server/Managers/PropertyManager.cs
+++ apps/server/Managers/PropertyManager.cs
@@ -858,7 +858,8 @@ public static class DefaultPropertyManager
         ("vitae_penalty", new Property<double>(0.05, "the amount of vitae penalty a player gets per death")),
         ("vitae_penalty_max", new Property<double>(0.40, "the maximum vitae penalty a player can have")),
         ("void_pvp_modifier", new Property<double>(0.5, "Scales the amount of damage players take from Void Magic. Defaults to 0.5, as per retail. For earlier content where DRR isn't as readily available, this can be adjusted for balance.")),
-        ("xp_modifier", new Property<double>(1.0, "scales the amount of xp received by players")));
+        ("xp_modifier", new Property<double>(1.0, "scales the amount of xp received by players")),
+        ("mana_cost_multiplier", new Property<double>(1.0, "scales mana costs for spells")));
 
     public static readonly ReadOnlyDictionary<string, Property<string>> DefaultStringProperties = DictOf(
         ("content_folder", new Property<string>("Content", "for content creators to live edit weenies. defaults to Content folder found in same directory as ACE.Server.dll")),
