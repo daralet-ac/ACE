1988d7e2c13cc649d1eb68b918be42dd70b53e7e
WarlockHolmez <105719338+WarlockHolmez@users.noreply.github.com>
2025-02-07 11:01:09 -0500

    feat: add LandblockName table for /listplayers improvements (#655)
    * feat: add landblock name table for /listplayers
    
    * drop id column
    
    * Update ListPlayers.cs
    
    * Update ListPlayers.cs

--- apps/server/Commands/DeveloperCommands/ServerCommands/ListPlayers.cs
+++ apps/server/Commands/DeveloperCommands/ServerCommands/ListPlayers.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
+using ACE.Database.Models.World;
 using ACE.Entity.Enum;
 using ACE.Server.Commands.Handlers;
+using ACE.Server.Entity;
 using ACE.Server.Managers;
 using ACE.Server.Network;
 
@@ -20,8 +23,7 @@ public class ListPlayers
     )]
     public static void HandleListPlayers(Session session, params string[] parameters)
     {
-        var message = "";
-        uint playerCounter = 0;
+        var message = " \n";
 
         AccessLevel? targetAccessLevel = null;
         if (parameters?.Length > 0)
@@ -54,6 +56,8 @@ public class ListPlayers
             message += $"Listing only {targetAccessLevel.Value.ToString()}s:\n";
         }
 
+        message += $"Total connected Players: {PlayerManager.GetAllOnline().Count}\n";
+
         foreach (var player in PlayerManager.GetAllOnline())
         {
             if (targetAccessLevel.HasValue && player.Account.AccessLevel != ((uint)targetAccessLevel.Value))
@@ -61,12 +65,51 @@ public class ListPlayers
                 continue;
             }
 
-            message += $"{player.Name} - Lv: {player.Level}, Acct: {player.Session.AccountId}\n";
-            playerCounter++;
+            var locationName = GetLocationName(player.Location.LandblockId.Raw) ?? player.Location.LandblockId.ToString();
+            var coordinates = player.Location.GetMapCoordStr();
+            var paranthesis = coordinates != null ? $" ({coordinates})" : " (Indoors)";
+
+            var location = $"{locationName}{paranthesis}";
+
+
+            message += $"{player.Name} ({player.Account.AccountName}/{player.Session.AccountId})  -  Lv: {player.Level}  -  Loc: {location}\n";
         }
 
-        message += $"Total connected Players: {playerCounter}\n";
 
-        CommandHandlerHelper.WriteOutputInfo(session, message, ChatMessageType.Broadcast);
+        CommandHandlerHelper.WriteOutputInfo(session, message, ChatMessageType.System);
+    }
+
+    private static string GetLocationName(uint cellId)
+    {
+        using var ctx = new WorldDbContext();
+
+        var query =
+            from landblockName in ctx.LandblockName
+            where
+                landblockName.ObjCellId == cellId
+            select new
+            {
+                Name = landblockName.Name
+            };
+
+        var name = query.ToList().FirstOrDefault()?.Name;
+
+        if (query.FirstOrDefault() is null)
+        {
+            var landblockId = (cellId | 0xFFFF) - 0xFFFF;
+
+            query =
+                from landblockName in ctx.LandblockName
+                where
+                    landblockName.ObjCellId == landblockId
+                select new
+                {
+                    landblockName.Name
+                };
+
+            name = query.ToList().FirstOrDefault()?.Name;
+        }
+
+        return name;
     }
 }
