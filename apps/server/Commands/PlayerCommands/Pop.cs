5e4bc5b6d6b92af99ab93eed70ad28aa8fc89db3
WarlockHolmez <warlockholmez@gmail.com>
2025-02-12 21:02:05 -0500

    Update Pop.cs

--- apps/server/Commands/PlayerCommands/Pop.cs
+++ apps/server/Commands/PlayerCommands/Pop.cs
@@ -13,12 +13,6 @@ public class Pop
     [CommandHandler("pop", AccessLevel.Player, CommandHandlerFlag.None, 0, "Show current world population", "")]
     public static void HandlePop(Session session, params string[] parameters)
     {
-        // CommandHandlerHelper.WriteOutputInfo(
-        //     session,
-        //     $"Current world population: {PlayerManager.GetOnlineCount():N0}",
-        //     ChatMessageType.Broadcast
-        // );
-
         ShowPop(session);
     }
 
