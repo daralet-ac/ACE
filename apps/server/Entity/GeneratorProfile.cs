f6904394d9b8eea2e99dfa86efbe5eee91845559
WarlockHolmez <warlockholmez@gmail.com>
2025-03-06 11:52:25 -0500

    feat: disable retail spawn logs

--- apps/server/Entity/GeneratorProfile.cs
+++ apps/server/Entity/GeneratorProfile.cs
@@ -322,9 +322,9 @@ public class GeneratorProfile
 
             if (wo is Creature { Attackable: true, UseArchetypeSystem: not true } creature)
             {
-                _log.Warning(
-                    "[GENERATOR] Preventing spawn of '{CreatureName}' ({CreatureWcid}) due to not using archetype system. Landblock: {Landblock}, Loc: {Loc}",
-                    creature.Name, creature.WeenieClassId, Generator.CurrentLandblock.Id, Generator.Location.ToLOCString());
+                // _log.Warning(
+                //     "[GENERATOR] Preventing spawn of '{CreatureName}' ({CreatureWcid}) due to not using archetype system. Landblock: {Landblock}, Loc: {Loc}",
+                //     creature.Name, creature.WeenieClassId, Generator.CurrentLandblock.Id, Generator.Location.ToLOCString());
 
                 return null;
             }
