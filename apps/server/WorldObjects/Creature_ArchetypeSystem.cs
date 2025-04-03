61bd1e25a763c797f9b44b3a94e27abb36b90b05
WarlockHolmez <105719338+WarlockHolmez@users.noreply.github.com>
2025-04-03 09:18:23 -0400

    feat: reduce enemy health (#756)

--- apps/server/WorldObjects/Creature_ArchetypeSystem.cs
+++ apps/server/WorldObjects/Creature_ArchetypeSystem.cs
@@ -12,7 +12,7 @@ partial class Creature
     private static bool DebugArchetypeSystem = false;
 
     // Stat ranges by tier
-    private static readonly int[] enemyHealth = { 10, 50, 150, 350, 500, 800, 1200, 1600, 2000 };
+    private static readonly int[] enemyHealth = { 10, 40, 100, 200, 300, 400, 500, 1000, 2000 };
     private static readonly int[] enemyStaminaMana = { 20, 100, 150, 225, 325, 450, 650, 950, 1250 };
     private static readonly int[] enemyHealthRegen = { 1, 2, 5, 10, 15, 20, 25, 30, 50 };
     private static readonly int[] enemyStaminaManaRegen = { 1, 2, 5, 10, 15, 20, 25, 30, 50 };
