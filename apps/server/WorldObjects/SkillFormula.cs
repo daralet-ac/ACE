975d3393c12eec727481339dcfbcddd4254f8bb3
WarlockHolmez <warlockholmez@gmail.com>
2025-05-03 06:15:33 -0400

    feat: rework shield damage reduction calculation
    Now based on shield level vs attacker attack skill.
    - When both are equal, provides 25% damage reduction.
    - Caps at 50%.
    - Minimum 0%.

--- apps/server/WorldObjects/SkillFormula.cs
+++ apps/server/WorldObjects/SkillFormula.cs
@@ -55,26 +55,6 @@ public class SkillFormula
     }
 
     /// <summary>
-    /// Converts SL from an additive linear value
-    /// to a scaled damage multiplier
-    /// </summary>
-    public static float CalcShieldMod(float shieldLevel)
-    {
-        if (shieldLevel > 0)
-        {
-            return ShieldMod / (shieldLevel + ShieldMod);
-        }
-        else if (shieldLevel < 0)
-        {
-            return 1.0f - shieldLevel / ShieldMod;
-        }
-        else
-        {
-            return 1.0f;
-        }
-    }
-
-    /// <summary>
     /// Converts Ward Level from an additive linear value
     /// to a scaled damage multiplier
     /// </summary>
@@ -91,6 +71,59 @@ public class SkillFormula
         else
         {
             return 1.0f;
+        }
+    }
+
+    private static readonly int[] avgEnemyAttackSkillPerTier = { 10, 50, 150, 200, 250, 300, 400, 500, 1000 };
+    private static readonly int[] avgShieldLevelPerTier = { 45, 90, 180, 270, 360, 450, 540, 630, 720 };
+
+    public static float CalcShieldMod(float shieldLevel, uint attackerSkill, uint attackerTier, int level)
+    {
+        var targetAttackSkill = GetTargetAttackSkill(attackerSkill, attackerTier, level);
+        var skillCheck = (float)SkillCheck.GetSkillChance((int)shieldLevel, (int)targetAttackSkill, 0.005f);
+
+        const float maxShieldMod = 0.5f;
+
+        //Console.WriteLine($"atk: {attackerSkill}, shield: {shieldLevel}, targetAttack: {targetAttackSkill}, mod: {1 - (skillCheck * maxShieldMod)}");
+
+        return 1 - (skillCheck * maxShieldMod);
+    }
+
+    private static double GetTargetAttackSkill(uint attackerSkill, uint attackerTier, int level)
+    {
+        var scalar = new double[8];
+        for (var i = 0; i < scalar.Length; i++)
+        {
+            scalar[i] = (double)avgShieldLevelPerTier[i] / avgEnemyAttackSkillPerTier[i];
+        }
+
+        var weightedScalar = scalar[attackerTier] +
+                             ((scalar[attackerTier + 1] - scalar[attackerTier]) * LevelWeight(attackerTier, level));
+
+        var targetAttackSkill = attackerSkill * weightedScalar;
+        return targetAttackSkill;
+    }
+
+    private static double LevelWeight(uint tier, int level)
+    {
+        switch (tier)
+        {
+            default:
+                return (double)level / 9;
+            case 1:
+                return (double)(level - 10) / 10;
+            case 2:
+                return (double)(level - 20) / 10;
+            case 3:
+                return (double)(level - 30) / 10;
+            case 4:
+                return (double)(level - 40) / 10;
+            case 5:
+                return (double)(level - 50) / 25;
+            case 6:
+                return (double)(level - 75) / 25;
+            case 7:
+                return (double)(level - 100) / 25;
         }
     }
 }
