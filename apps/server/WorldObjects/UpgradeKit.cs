29ecd775fd4bebb3b18dd4ea67b7082d3e9ced77
WarlockHolmez <warlockholmez@gmail.com>
2025-12-15 23:51:05 -0500

    fix: resto mod upgrade kit

--- apps/server/WorldObjects/UpgradeKit.cs
+++ apps/server/WorldObjects/UpgradeKit.cs
@@ -412,12 +412,12 @@ public class UpgradeKit : Stackable
         if (magicSkill == Skill.WarMagic)
         {
             target.SetProperty(PropertyFloat.ElementalDamageMod, final);
-            target.SetProperty(PropertyFloat.WeaponRestorationSpellsMod, 1 + (final - 1) / 2);
+            target.SetProperty(PropertyFloat.WeaponRestorationSpellsMod, 1 + (final - 1) / 4);
         }
         else
         {
-            target.SetProperty(PropertyFloat.WeaponRestorationSpellsMod, final);
             target.SetProperty(PropertyFloat.ElementalDamageMod, 1 + (final - 1) / 2);
+            target.SetProperty(PropertyFloat.WeaponRestorationSpellsMod, 1 + (final - 1) / 2);
         }
     }
 
