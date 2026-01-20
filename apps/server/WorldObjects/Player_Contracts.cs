5505db69ff24218924ad78d7c6f9e6e7500f9520
WarlockHolmez <105719338+WarlockHolmez@users.noreply.github.com>
2026-01-19 22:40:48 -0500

    feat: update contract system (#1033)
    * feat: allow items to quest stamp player on purchased
    
    * Update Player_Commerce.cs
    
    * Update Player_Contracts.cs
    
    * Update ContractId.cs

--- apps/server/WorldObjects/Player_Contracts.cs
+++ apps/server/WorldObjects/Player_Contracts.cs
@@ -1,9 +1,41 @@
+using System.Collections.Generic;
+using ACE.Entity.Enum;
+using ACE.Server.Network.GameMessages.Messages;
+
 namespace ACE.Server.WorldObjects;
 
 partial class Player
 {
+    /// <summary>
+    /// Contract IDs that cannot be abandoned by players
+    /// </summary>
+    private static readonly HashSet<ContractId> NonAbandonableContracts = new HashSet<ContractId>
+    {
+        ContractId.Contract_323_Training_Academy
+    };
+
     public void HandleActionAbandonContract(uint contractId)
     {
+        // Check if this contract can be abandoned
+        if (NonAbandonableContracts.Contains((ContractId)contractId))
+        {
+            // Allow abandoning if the contract is Done
+            var contractTracker = ContractManager.GetContractTracker(contractId);
+            if (contractTracker == null || contractTracker.Stage != Network.Structure.ContractStage.DoneOrPendingRepeat)
+            {
+                var datContract = ContractManager.GetContractFromDat(contractId);
+                var contractName = datContract?.ContractName ?? "This task";
+
+                Session.Network.EnqueueSend(
+                    new GameMessageSystemChat(
+                        $"The '{contractName}' task cannot be abandoned until completed.",
+                        ChatMessageType.Broadcast
+                    )
+                );
+                return;
+            }
+        }
+
         ContractManager.Abandon(contractId);
     }
 }
