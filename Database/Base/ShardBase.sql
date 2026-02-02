76bc8344335bbcacdaf6c7013f6fe9ff5d8d7ba1
WarlockHolmez <warlockholmez@gmail.com>
2026-02-01 22:38:48 -0500

    Update ShardBase.sql

--- Database/Base/ShardBase.sql
+++ Database/Base/ShardBase.sql
@@ -977,8 +977,6 @@ CREATE TABLE IF NOT EXISTS `player_market_transactions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-
-
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
